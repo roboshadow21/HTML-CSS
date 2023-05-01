91eb7916333bb7cb4ec353dfaf7909bb8f25b5b7
Alexei Yurikov <roboshadow@mail.ru>
2023-05-01 11:18:36 +0300

    added some feature to index.html

--- Online_Store/script.js
+++ Online_Store/script.js
@@ -1,18 +1,7 @@
-const bannerToggleMenu = document.querySelector('.banner__toggle__menu'); // Находим меню
-
-// const menuActive = document.querySelector('.menu-active'); // Находим меню
-// console.log(banner__toggle__menu);
-// console.log(menuActive);
-
-const headerButton = document.querySelector('.btn__burger'); // Находим кнопку бургера
-
-// const headerMenu = document.querySelector('.user-list__item-menu'); // Находим кнопку бургера
-
-// let menuClose = document.querySelector('.menu-close'); // Находим кнопку крестика
-
+const bannerToggleMenu = document.querySelector('.banner__toggle__menu'); 
+const headerButton = document.querySelector('.btn__burger');
 function toggleMenu() {
-	bannerToggleMenu.classList.toggle('hidden'); //Функция удаляет или устанавливает класс 'hidden' в div с классом 'menu-active'
+	bannerToggleMenu.classList.toggle('hidden'); 
 }
 
-headerButton.addEventListener('click', toggleMenu); // По клику на бургер срабатывает функция
-// menuClose.addEventListener('click', toggleMenu); // По клику на крестик срабатывает функция
+headerButton.addEventListener('click', toggleMenu); 
